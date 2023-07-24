eb516349a6df25b3e83f73abfc984cb6e578a50b
Luke Tetteh <luketetteh7@gmail.com>
2023-07-24 21:52:56 +0000

    api

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Exports to-do list information of all employees to JSON format."""
+"""Exports to-do list information of all employees to JSON format"""
 import json
 import requests
 
@@ -8,7 +8,11 @@ if __name__ == "__main__":
     users = requests.get(url + "users").json()
 
     with open("todo_all_employees.json", "w") as jsonfile:
-        json.dump({u.get("id"):[{
+        json.dump({
+            u.get("id"): [{
                 "task": t.get("title"),
                 "completed": t.get("completed"),
-                "username": u.get("username") } for t in requests.get(url + "todos", params={"userId": u.get("id")}).json()] for u in users}, jsonfile)
+                "username": u.get("username")
+                } for t in requests.get(url + "todos",
+                    params={"userId": u.get("id")}).json()]
+                for u in users}, jsonfile)
