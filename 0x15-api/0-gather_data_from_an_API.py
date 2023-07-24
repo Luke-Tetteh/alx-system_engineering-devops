01a510e4d202156c76a4c8d459ae94429c32f5e8
Luke Tetteh <luketetteh7@gmail.com>
2023-07-24 21:23:05 +0000

    api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Returns to-do list information for a given employee ID."""
+"""Grabing user data using api."""
 import requests
 import sys
 
--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-"""Exports to-do list information for a given employee ID to CSV format."""
+""" Exports to-do list information for a particular employee ID to csv"""
 import csv
 import requests
 import sys
 
-if __name__ == "__main__":
+if __name__ = "__main__":
     user_id = sys.argv[1]
     url = "https://jsonplaceholder.typicode.com/"
     user = requests.get(url + "users/{}".format(user_id)).json()
-    username = user.get("username")       
-    todos = requests.get(url + "todos", params={"userId": user_id}).json()
+    username = user.get("username")
+    todos = requets.get(url + "todos", params={"userId": user_id}).json()
 
-    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
+    with open("{}.csv".format(user_id), "w", newlines="") as csvfile:
         writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
         [writer.writerow(
             [user_id, username, t.get("completed"), t.get("title")]
