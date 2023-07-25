7a176b536782bcfc058cf606c669ed5d1d78d7b4
Luke Tetteh <luketetteh7@gmail.com>
2023-07-25 06:01:08 +0000

    task 3

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,19 +1,39 @@
-#!usr/bin/python3
-"""Exports to-do list information of all employees to JSON format"""
+#!/usr/bin/python3
+"""
+This script uses the JSON placeholder REST API,
+to fetch information for all employees it
+returns information about their TODO list progress.
+
+Usage: ./3-dictionary_of_list_of_dictionaries.py
+
+The script exports data of the employee's todo  in the JSON format:
+    {"USER_ID":
+    [{"task": "TASK_TITLE", "completed": TASK_COMPLETED_STATUS,
+    "username": "USERNAME"},
+    {"task": "TASK_TITLE", "completed": TASK_COMPLETED_STATUS,
+    "username": "USERNAME"}, ... ]}"
+    -Saves them in a file todo_all_employees.json
+"""
 import json
 import requests
 import sys
 
-if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
-    users = requests.get(url + "users").json()
 
-    with open("todo_all_employees.json", "w") as jsonfile:
-        json.dump({
-            u.get("id"): [{
-                "task": t.get("title"),
-                "completed": t.get("completed"),
-                "username": u.get("username")
-             } for t in requests.get(url + "todos", 
-                                    params={"userId": u.get("id")}).json()]
-             for u in users}, jsonfile) 
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com"
+    users = requests.get("{}/users".format(url)).json()
+    tasks = requests.get("{}/{}".format(url, "todos")).json()
+    json_data = {}
+    for user in users:
+        data = []
+        for task in tasks:
+            if task.get("userId") == user.get("id"):
+                data.append({
+                    "task": task.get("title"),
+                    "completed": task.get("completed"),
+                    "username": user.get("username"),
+                    })
+        if data:
+            json_data[user.get("id")] = data
+    with open('todo_all_employees.json', 'w', encoding='UTF8') as f:
+        json.dump(json_data, f)
